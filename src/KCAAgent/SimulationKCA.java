061b8e22132a2360cbb361cfc714524917cbdbc5
andreiolaru <cs@andreiolaru.ro>
2015-01-13 18:14:24 +0200

    Use WindowLayout jar instead of linking project.

--- src/KCAAgent/SimulationKCA.java
+++ src/KCAAgent/SimulationKCA.java
@@ -46,60 +46,63 @@ import base.scenario.AbstractScenario;
 /**
  * Main class for managing a KCA-type simulation
  */
-public class SimulationKCA extends Simulation<EnvironmentKCA, CommandKCA> {
+public class SimulationKCA extends Simulation<EnvironmentKCA, CommandKCA>
+{
 	/**
 	 * The serial version UID.
 	 */
 	private static final long					serialVersionUID	= 1L;
-
+	
 	/**
 	 * The name of the scenario to use.
 	 */
 	// public final static String scenarioName = "scenarios/kcaScenario.xml";
 	// public final static String scenarioName = "scenarios/ooo.xml";
 	public final static String					scenarioName		= "scenarios/kca_test3r.xml";
-
+	
 	KCAScenario									scenario			= new KCAScenario(scenarioName);
-
+	
 	private DataContent[]						data				= scenario.getData();
-
+	
 	private static StepNumber					sn					= new StepNumber();
 	private static JSlider						sw					= new StepDuration();
-
+	
 	private ControllableView<EnvironmentKCA>[]	viewers				= null;
-
+	
 	@SuppressWarnings("unused")
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		new SimulationKCA();
 	}
-
-	public SimulationKCA() {
+	
+	public SimulationKCA()
+	{
 		commands = scenario.getCommands();
-
+		
 		init1();
-
+		
 		RowsLayout layout = new RowsLayout((RowsLayoutIndications) new RowsLayoutIndications()
-				.indicateNMainsIndications(1).indicateMainPercent(30).indicateRowCount(5)
-				.makeSquare().indicateBar(BarPosition.LEFT, 100, 0)
-				.indicateBar(BarPosition.TOP, 70, 0).indicateW(1050).indicateH(900));
-
+				.indicateNMainsIndications(1).indicateControlH(70).indicateMainPercent(30).indicateRowCount(5)
+				.makeSquare().indicateBar(BarPosition.LEFT, 100, 0).indicateBar(BarPosition.TOP, 70, 0).indicateW(1050)
+				.indicateH(900));
+		
 		// larger (1680), windows 7
 		// WindowLayout layout = new WindowLayout(70, 0, 1600, 1000, 15, 1, 5,
 		// true, true);
-
+		
 		// large (1280), windows 7
 		// WindowLayout layout = new WindowLayout(70, 0, 1200, 800, 20, 1, 5,
 		// true, true);
-
+		
 		// small (1024)
 		// WindowLayout layout = new WindowLayout(0, 0, 1000, 600, 60, 1, 5,
 		// true, true);
-
+		
 		layout.addRow(base.graphics.ViewerFactory.Type.FACTS_GRID.toString(), data);
-
+		
 		String[] types = new String[4 + Domain.values().length];
 		int i = 0;
-		for (@SuppressWarnings("unused")
+		for(@SuppressWarnings("unused")
 		Domain dom : Domain.values())
 			types[i++] = base.graphics.ViewerFactory.Type.DOMAIN_INTEREST_GRID.toString();
 		types[i++] = base.graphics.ViewerFactory.Type.SPECIALTY_GRID.toString();
@@ -107,52 +110,51 @@ public class SimulationKCA extends Simulation<EnvironmentKCA, CommandKCA> {
 		types[i++] = base.graphics.ViewerFactory.Type.AGENT_SELECTION_GRID.toString();
 		types[i++] = base.graphics.ViewerFactory.Type.PAUSE_GRID.toString();
 		layout.addRow(-1, -1, types, Domain.values());
-
+		
 		GraphParam[] param = new GraphParam[data.length];
 		i = 0;
-		for (Object datum : data)
-			param[i++] = new AbstractGraphViewer.GraphParam(null,
-					new AbstractGraphViewer.GraphLink("LF"), 1, datum);
-
+		for(Object datum : data)
+			param[i++] = new AbstractGraphViewer.GraphParam(null, new AbstractGraphViewer.GraphLink("LF"), 1, datum);
+		
 		layout.addRow(base.graphics.ViewerFactory.Type.GLOBAL_FACT_NUMBER_GRAPH.toString(), param);
-
-		layout.addRow(new String[] {
-				base.graphics.ViewerFactory.Type.GLOBAL_PRESSURE_GRAPH.toString(),
+		
+		layout.addRow(new String[] { base.graphics.ViewerFactory.Type.GLOBAL_PRESSURE_GRAPH.toString(),
 				base.graphics.ViewerFactory.Type.MAX_PRESSURE_GRAPH.toString(),
 				base.graphics.ViewerFactory.Type.MESSAGE_AVG_GRAPH.toString(),
 				base.graphics.ViewerFactory.Type.USELESS_FACTS_AVG_GRAF.toString(),
 				base.graphics.ViewerFactory.Type.AGENT_BALANCE_AVG_GRAF.toString(), });
-
-		Object[] items = new Object[]{null, GoalType.INFORM, /* GoalType.GET,*/ GoalType.FREE};
+		
+		Object[] items = new Object[] { null, GoalType.INFORM, /* GoalType.GET, */GoalType.FREE };
 		GraphParam[] param1 = new GraphParam[items.length];
 		i = 0;
-		for (Object item : items)
-			param1[i++] = new AbstractGraphViewer.GraphParam(null,
-					new AbstractGraphViewer.GraphLink("LG"), 1, item);
+		for(Object item : items)
+			param1[i++] = new AbstractGraphViewer.GraphParam(null, new AbstractGraphViewer.GraphLink("LG"), 1, item);
 		layout.addRow(base.graphics.ViewerFactory.Type.GLOBAL_GOAL_NUMBER_GRAPH.toString(), param1);
 		
 		layout.addMain(-1, 100, base.graphics.ViewerFactory.Type.CONTROL.toString(), this);
 		layout.addMain(-1, -1, base.graphics.ViewerFactory.Type.LOG_VIEWER.toString(), null);
-
+		
 		// layout.addMain(new WindowParameters(Type.AGENT_DETAILS, -1, -1, 0,
 		// 0));
-
+		
 		viewers = ViewerFactoryKCA.createViewers(environment, layout.getAllWindows());
-
+		
 		init2();
 	}
-
+	
 	@Override
-	public void createMainWindow(int x, int y, int w, int h) {
+	public void createMainWindow(int x, int y, int w, int h)
+	{
 		this.setLayout(new BorderLayout());
-
+		
 		Panel box = new Panel();
 		box.setLayout(new BoxLayout(box, BoxLayout.LINE_AXIS));
 		box.setBackground(Color.white);
 		JButton start = new JButton("Start");
 		start.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e)
+			{
 				start();
 			}
 		});
@@ -160,7 +162,8 @@ public class SimulationKCA extends Simulation<EnvironmentKCA, CommandKCA> {
 		JButton stop = new JButton("Stop");
 		stop.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e)
+			{
 				stop();
 			}
 		});
@@ -168,18 +171,20 @@ public class SimulationKCA extends Simulation<EnvironmentKCA, CommandKCA> {
 		JButton step = new JButton("Step");
 		step.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e)
+			{
 				step();
 			}
 		});
 		box.add(step);
-
+		
 		box.add(sn);
-
+		
 		final JButton randomize = new JButton("Randomize " + AbstractScenario.getSeed());
 		randomize.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e)
+			{
 				AbstractScenario.resetRandom();
 				randomize.setText("Randomize " + AbstractScenario.getSeed());
 			}
@@ -188,33 +193,36 @@ public class SimulationKCA extends Simulation<EnvironmentKCA, CommandKCA> {
 		JButton save = new JButton("Save");
 		save.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e)
+			{
 				JFileChooser fc = new JFileChooser(".");
-
-				if (fc.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+				
+				if(fc.showSaveDialog(null) == JFileChooser.APPROVE_OPTION)
+				{
 					scenario.save(fc.getSelectedFile());
 				}
 			}
 		});
 		box.add(save);
-
+		
 		JButton override = new JButton("Overwrite");
 		override.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e)
+			{
 				String scenarioNameMod = new String(scenarioName);
 				scenarioNameMod = scenarioNameMod.replaceAll(".xml", "-mod.xml");
 				scenario.save(new File(scenarioNameMod));
 			}
 		});
 		box.add(override);
-
+		
 		this.add(box, BorderLayout.CENTER);
-
+		
 		box = new Panel();
 		box.add(sw);
 		this.add(box, BorderLayout.SOUTH);
-
+		
 		this.setTitle("KCA");
 		this.setLocation(x, y);
 		this.setSize(w, h);
@@ -222,65 +230,83 @@ public class SimulationKCA extends Simulation<EnvironmentKCA, CommandKCA> {
 		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		this.setVisible(true);
 	}
-
-	private void init1() {
+	
+	private void init1()
+	{
 		environment = new EnvironmentKCA(this, scenario);
 		environment.getLogger().setLevel(LEVEL);
 		log = new Log(null);
 		environment.getLogger().addLog(log);
 		nextcommand = 0;
 	}
-
-	private void init2() {
+	
+	private void init2()
+	{
 		environment.addUpdateListener(this);
 		environment.addUpdateListener(sn);
-
-		for (ControllableView<EnvironmentKCA> viewer : viewers)
-			if (viewer != null)
+		
+		for(ControllableView<EnvironmentKCA> viewer : viewers)
+			if(viewer != null)
 				viewer.relink(environment);
-
+		
 		environment.doUpdate();
 	}
-
+	
 	@Override
-	protected void doCommand(CommandKCA command) {
-		if (command.getAction() == Command.Action.INJECT) {
+	protected void doCommand(CommandKCA command)
+	{
+		if(command.getAction() == Command.Action.INJECT)
+		{
 			log.le("injecting ~ at ~", command.getFact(), command.getLocation());
-			AgentID receiver = environment.inject(command.getLocation(), new MessageKCA(null,
-					MessageKCA.Type.INFORM, command.getFact().toCollection()));
-			if (receiver != null)
+			AgentID receiver = environment.inject(command.getLocation(), new MessageKCA(null, MessageKCA.Type.INFORM,
+					command.getFact().toCollection()));
+			if(receiver != null)
 				log.le("received by ~", receiver);
 			else
 				log.le("received by noone, every agents in pause");
-		} else if (command.getAction() == Command.Action.REQUEST) {
+		}
+		else if(command.getAction() == Command.Action.REQUEST)
+		{
 			log.le("requesting ~ from ~", command.getFact(), command.getLocation());
-			environment.inject(command.getLocation(), new MessageKCA(null, MessageKCA.Type.REQUEST,
-					command.getFact().toCollection()));
-		} else if (command.getAction() == Command.Action.SNAPSHOT) {
+			environment.inject(command.getLocation(), new MessageKCA(null, MessageKCA.Type.REQUEST, command.getFact()
+					.toCollection()));
+		}
+		else if(command.getAction() == Command.Action.SNAPSHOT)
+		{
 			// doSnapshot("data_" + command.fact.getData().getId() + "_" + step
 			// + ".txt", command.fact.getData().getId());
 			// log.li("Snapshot on ~", command.fact.getData().getId());
-		} else if (command.getAction() == Command.Action.PAUSE) {
+		}
+		else if(command.getAction() == Command.Action.PAUSE)
+		{
 			// if(!command.getAgent().isPause())
 			command.getAgent().getLog().lf("agents paused ");
 			command.getAgent().pause();
-		} else if (command.getAction() == Command.Action.UNPAUSE) {
+		}
+		else if(command.getAction() == Command.Action.UNPAUSE)
+		{
 			// if(command.getAgent().isPause())
 			command.getAgent().getLog().lf("agents unpaused ");
 			command.getAgent().unpause();
-		} else if (command.getAction() == Command.Action.MOVE) {
+		}
+		else if(command.getAction() == Command.Action.MOVE)
+		{
 			LocationAgent agent = (LocationAgent) command.getAgent();
 			agent.setLocation(command.getLocation());
 		}
 	}
-
-	void doSnapshot(String name) {
+	
+	void doSnapshot(String name)
+	{
 		PrintStream ps = null;
-		try {
+		try
+		{
 			ps = new PrintStream(name);
 			ps.println("\\begin{verbatim}");
-			for (int i = 0; i < scenario.getHeight(); i++) {
-				for (int j = 0; j < scenario.getWidth(); j++) {
+			for(int i = 0; i < scenario.getHeight(); i++)
+			{
+				for(int j = 0; j < scenario.getWidth(); j++)
+				{
 					// boolean contains = cm.cellAt(i, j).getData().contains(new
 					// DataContent(new Data(dataID), 0));
 					// ps.print(contains ? "." : " ");
@@ -289,59 +315,73 @@ public class SimulationKCA extends Simulation<EnvironmentKCA, CommandKCA> {
 			}
 			ps.println("\\end{verbatim}");
 			ps.close();
-		} catch (FileNotFoundException e) {
+		} catch(FileNotFoundException e)
+		{
 			e.printStackTrace();
 		}
-
+		
 	}
-
-	void doReply(Message<?> msg) {
+	
+	void doReply(Message<?> msg)
+	{
 		log.le("received ~", msg);
 	}
-
+	
 	@Override
-	public void run() {
+	public void run()
+	{
 		int step = Environment.getStep();
-		while (step < scenario.getNsteps() && active) {
+		while(step < scenario.getNsteps() && active)
+		{
 			step = Environment.getStep();
-
-			if (step == LEVELSWITCH)
+			
+			if(step == LEVELSWITCH)
 				environment.getLogger().setLevel(LEVELTO);
-
-			if (step % PRINTSTEP == 0) {
+			
+			if(step % PRINTSTEP == 0)
+			{
 				log.le("===================== STEP ~ ======================", new Integer(step));
-			} else {
+			}
+			else
+			{
 				log.li("===================== STEP ~ ======================", new Integer(step));
 			}
-
+			
 			// for(int i = 0; i < absCommands.length; i++)
 			// if(absCommands[i].time == step)
 			// doCommand(absCommands[i]);
-
-			while (nextcommand < commands.length && commands[nextcommand].getTime() == step) {
+			
+			while(nextcommand < commands.length && commands[nextcommand].getTime() == step)
+			{
 				doCommand(commands[nextcommand++]);
 			}
-			try {
+			try
+			{
 				environment.step();
-			} catch (Exception e1) {
+			} catch(Exception e1)
+			{
 				e1.printStackTrace();
 			}
-			try {
+			try
+			{
 				Thread.sleep(sw.getValue());
-			} catch (InterruptedException e) {
+			} catch(InterruptedException e)
+			{
 				e.printStackTrace();
 			}
-			if (oneStep) {
+			if(oneStep)
+			{
 				oneStep = false;
 				break;
 			}
 		}
 		active = false;
 	}
-
+	
 	@Override
-	public void update() {
+	public void update()
+	{
 		this.setTitle("KCA - " + Environment.getStep());
 	}
-
+	
 }
